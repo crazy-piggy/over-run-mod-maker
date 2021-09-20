0894c92a2d1cdd8413d2530c7de181dccb9a6515
beanflame <68175497+beanflame@users.noreply.github.com>
2021-09-20 16:28:49 +0800

    Update MangoModMaker.java

--- src/main/java/io/github/mangostudio/mango/MangoModMaker.java
+++ src/main/java/io/github/mangostudio/mango/MangoModMaker.java
@@ -1,10 +1,10 @@
 package io.github.mangostudio.mango;
 
 public class MangoModMaker {
-    public static final String NAME = "Mango Mod Maker";
+    public static final String NAME = "Over Run Mod Maker";
     public static final String VERSION = "0.1.0-Alpha";
-    public static final String DONATE_URL = "https://afdian.net/@beanflame";
-    public static final String GITEE_URL = "https://gitee.com/mango-studio/mango-mod-maker";
-    public static final String GITHUB_URL = "https://github.com/mango-studio/mango-mod-maker";
-    public static final String UPDATE_LOG_URL = "https://shimo.im/docs/DtCrHYxvHCdGHrhq/";
+    public static final String DONATE_URL = "https://over-run.github.io";
+    public static final String GITEE_URL = "https://gitee.com/Over-Run/over-run-mod-maker";
+    public static final String GITHUB_URL = "https://github.com/Over-Run/over-run-mod-maker";
+    public static final String UPDATE_LOG_URL = "https://over-run.github.io";
 }
